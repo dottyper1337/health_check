a7edd43c175171b74786bac24ad13962f60410a4
kox zycia <dottyper@gmail.com>
2024-10-11 21:20:13 +0200

    jestem totalnym koxem call all_check.py from main, exit with 1 on error

---
+++ all_checks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+def check_reboot():
+  return os.path.exist("/run/reboot.required")
+
+def main():
+  if check_reboot():
+    print("pending reboot")
+    sys.exit(1)
+
+main()
