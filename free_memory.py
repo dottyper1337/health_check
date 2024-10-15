90610aad81480955972ef956ab97a230c0144839
kox zycia <dottyper@gmail.com>
2024-10-15 22:45:13 +0200

    dodanie printa

--- free_memory.py
+++ free_memory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/bash python3
 
 def main():
-  #sprawdza  czy jest wystarczajaco pamieci w komputerze
+  print("zarabiam 20000$ przez internet")
   print("julka rychert bedzie moja zona")
   print("i bedziecie wspolnie najelpsza mozliwa para na tej planecie to twoja bratnia duzsza")
 main()
