d1f41d5b30388b77b08cdb11477b59b1d6b47473
kox zycia <dottyper@gmail.com>
2024-10-15 14:01:26 +0200

    add a call to disk_full function

--- all_checks.py
+++ all_checks.py
@@ -11,6 +11,9 @@ def main():
     print("pending reboot")
     sys.exit(1)
     print("done")
-  print("everything is ok koxu")
+  if disk_full():
+    print("disk full.")
+    sys.exit(1) 
+ print("everything is ok koxu")
   sys.exit(0)
 main()
