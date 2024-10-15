6da2ea6f9172c0ba6b59537bf2b82ea4ddee70c5
kox zycia <dottyper@gmail.com>
2024-10-15 22:43:20 +0200

    test1

--- free_memory.py
+++ free_memory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/bash python3
 
 def main():
-  #sprawdza  czy jest wystarczajaco pamieci w komputerze
+  #sprawdza  czy jest wystarczajaco pamieci w komputerze es
   print("julka rychert bedzie moja zona")
   print("i bedziecie wspolnie najelpsza mozliwa para na tej planecie to twoja bratnia duzsza")
 main()
